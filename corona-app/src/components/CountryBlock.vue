8619d6d9685906b37373f865c5adb4ba4a04750a
Mateusz <mateuszglita@gmail.com>
2020-04-24 11:31:35 +0200

    display list of countries

---
+++ corona-app/src/components/CountryBlock.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="block">
+        <span class="flag"><img :src="getImgUrl(flag)"></span>
+
+        <span class="country">{{country}}</span>
+        <span>Liczba zakażonych: 19022</span>
+    </div>    
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class CountryBlock extends Vue {
+  @Prop() private country!: string;
+  @Prop() private flag!: string;
+  @Prop() private count!: number;   
+  getImgUrl(pic:string) {
+    return require('../assets/'+pic)
+}
+}
+
+</script>
+
+<style scoped>
+.block{
+    flex: 0 0 26%;
+    margin-bottom: 70px;
+    width: 300px;
+    height: 70px;
+    border-radius: 10px;
+    background: #f2f2f2;
+    box-shadow: 0 5px 10px 0 rgba(0, 0,0,.3);
+    cursor: pointer;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    transition: 0.3s;
+}
+.block:hover{
+    transform: scale(1.1);
+}
+.country{
+    font-weight: 550;
+    margin-left: 10px;
+    margin-right: 10px;
+}
+.flag{
+    margin-left: 5px;
+}
+</style>
--- corona-app/src/views/World.vue
+++ corona-app/src/views/World.vue
@@ -1,5 +1,65 @@
 <template>
   <div class="world">
-    <h1>Świat</h1>
+    <div class="header"><p>Świat</p></div>
+    <div class="secondary"><p>Strona przedstawiająca zachorowania w wybranych krajach</p></div>
+    <div class="countries">
+      <CountryBlock v-for="item in countries" v-bind:key="item.id" :country="item.name" :flag="item.flag"></CountryBlock>
+    </div>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import CountryBlock from '@/components/CountryBlock.vue'
+
+export default {
+  name: 'World',
+  components: {
+    CountryBlock
+  },
+  data(){
+    return{
+    countries:[
+      {
+        "name":"Hiszpania",
+        "flag":"spain.png"
+      },
+      {
+        "name":"Portugalia",
+        "flag":"portugal.png"
+      },
+      { 
+        "name":"Francja",
+        "flag":"france.png"
+      },
+      {
+        "name":"Niemcy",
+        "flag":"germany.png"
+      },
+      {
+        "name":"Wielka Brytania",
+        "flag":"britain.png"
+      }
+    ]}
+  }
+}
+</script>
+<style scoped>
+.world{
+  margin-top:30px;
+}
+.header{
+  font-size: 70px;
+  font-weight: 500;
+}
+.secondary{
+  font-size: 30px;
+  font-weight: 300;
+  margin-top:60px;
+}
+.countries{
+  margin-top: 100px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+</style>
